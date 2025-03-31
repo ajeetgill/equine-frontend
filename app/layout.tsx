de7fa611ea094c6f9021b354ffdd802ccf128d91
Ajeet Gill <ajeetshergill@gmail.com>
2025-03-31 01:26:37 -0300

    icons & metadata of website fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import logo from "@/assets/logo-small.png";
 import Link from "next/link";
 import "./globals.css";
+import Image from "next/image";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -11,8 +13,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Horse C.O.P. Reports",
+  description:
+    "The Website to download the reports generated from accompanying mobile app.",
 };
 
 const geistSans = Geist({
@@ -38,9 +41,17 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Equineee</Link>
-                  </div>
+                  <Link
+                    href={"/"}
+                    className="flex items-center text-lg font-black"
+                  >
+                    <Image
+                      src={logo}
+                      className="w-auto max-w-[35px]"
+                      alt="Logo for Horse Assessment Report Downloading WebApp"
+                    />
+                    C.O.P
+                  </Link>
                   <div className="flex">
                     <HeaderAuth />
                     <ThemeSwitcher />
